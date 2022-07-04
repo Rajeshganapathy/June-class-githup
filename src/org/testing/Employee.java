229bc30984f7b5b5dbdeb84965a4058861c652c4
Rajeshganapathy <rajeshganapathy0303@gmail.com>
2022-07-04 15:17:18 +0530

    update empName method

---
+++ src/org/testing/Employee.java
@@ -0,0 +1,27 @@
+package org.testing;
+
+public class Employee {
+	
+	
+		public void empName() {
+			System.out.println("Raja");
+
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+
+}
